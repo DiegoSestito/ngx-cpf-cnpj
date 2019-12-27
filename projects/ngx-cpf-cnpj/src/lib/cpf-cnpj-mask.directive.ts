ffcc16adc53b242e89634490f654c6e0cd68bb3c
Diego Sestito <diegosestito@gmail.com>
2019-12-27 11:41:41 -0300

    Error correction on cnpj mask, issue 'Error applying mask'

--- projects/ngx-cpf-cnpj/src/lib/cpf-cnpj-mask.directive.ts
+++ projects/ngx-cpf-cnpj/src/lib/cpf-cnpj-mask.directive.ts
@@ -1,17 +1,21 @@
-import { Directive, HostListener } from '@angular/core';
-import { NgModel } from '@angular/forms';
-import { NgxCpfCnpjService } from './ngx-cpf-cnpj.service';
+import { Directive, HostListener } from "@angular/core";
+import { NgModel } from "@angular/forms";
+import { NgxCpfCnpjService } from "./ngx-cpf-cnpj.service";
 @Directive({
-  selector: '[nccCpfCnpjMask] [ngModel]',
+  selector: "[nccCpfCnpjMask] [ngModel]",
   providers: [NgModel]
 })
 export class CpfCnpjMaskDirective {
-  constructor(private model: NgModel, private cpfCnpjService: NgxCpfCnpjService) { }
+  constructor(
+    private model: NgModel,
+    private cpfCnpjService: NgxCpfCnpjService
+  ) {}
 
-  @HostListener('input', ['$event']) onInput(event) {
-    if (event.target.value.length <= 17) {
-      this.model.valueAccessor.writeValue(this.cpfCnpjService.convertToCpfCnpj(event.target.value));
+  @HostListener("input", ["$event"]) onInput(event) {
+    if (event.target.value.length <= 18) {
+      this.model.valueAccessor.writeValue(
+        this.cpfCnpjService.convertToCpfCnpj(event.target.value)
+      );
     }
   }
-
 }
