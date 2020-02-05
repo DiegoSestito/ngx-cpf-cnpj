ed82f5f60301993b87696dfbb387c2ceaee2916b
Diego Cesar Sestito <diego.sestito@pca.com.br>
2020-02-05 09:51:51 -0300

    Alteração do projeto de exemplo: Implementação de exempl com ReactiveForm

--- projects/ncc-exemple/src/app/app.module.ts
+++ projects/ncc-exemple/src/app/app.module.ts
@@ -1,20 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { NgxCpfCnpjModule } from 'ngx-cpf-cnpj';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { NgxCpfCnpjModule } from "ngx-cpf-cnpj";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    NgxCpfCnpjModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, NgxCpfCnpjModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
