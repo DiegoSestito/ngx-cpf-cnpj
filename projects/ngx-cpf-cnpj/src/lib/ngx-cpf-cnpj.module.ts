e84ca85491be0511e754cbb8ad155a01f9290a8d
Diego Cesar Sestito <diego.sestito@pca.com.br>
2020-02-05 11:23:40 -0300

    Correção de exportação do validador.

--- projects/ngx-cpf-cnpj/src/lib/ngx-cpf-cnpj.module.ts
+++ projects/ngx-cpf-cnpj/src/lib/ngx-cpf-cnpj.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CpfCnpjMaskDirective } from "./cpf-cnpj-mask.directive";
 import { CpfCnpjValidatorDirective } from "./cpf-cnpj-validator.directive";
+import { CpfCnpjValidator } from "./cpf-cnpj.validator";
 
 @NgModule({
-  declarations: [CpfCnpjMaskDirective, CpfCnpjValidatorDirective],
+  declarations: [
+    CpfCnpjMaskDirective,
+    CpfCnpjValidatorDirective,
+    CpfCnpjValidator
+  ],
   imports: [],
-  exports: [CpfCnpjMaskDirective, CpfCnpjValidatorDirective]
+  exports: [CpfCnpjMaskDirective, CpfCnpjValidatorDirective, CpfCnpjValidator]
 })
 export class NgxCpfCnpjModule {}
